24c507e09ca9c60ebd4ea27912a29696b6dc278f
Santosh Kumawat <santoshkumaawat@gmail.com>
2024-05-30 17:15:34 +0530

    04 - props

---
+++ src/04-props/Props.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PropsEx = () => {
+    return (
+        <div>
+            <h1>Props In React</h1>
+            <p>Props are like parameters to a function</p>
+
+            <img src="/04-props/props.png" alt="" srcset="" />
+        </div>
+    );
+}
+
+export default PropsEx;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Component from "./01-components/Component";
 import HorizontalLine from "./01-components/HorizontalLine";
 import JSX from "./02-jsx/JSX";
 import ToDoApp from "./03-todo-app/ToDoApp";
+import PropsEx from "./04-props/Props";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       <ToDoApp/> 
       <HorizontalLine/>
 
-      
+      <PropsEx/>
+      <HorizontalLine/>
 
     </div>
   );
